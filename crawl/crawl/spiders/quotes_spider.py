bebe227370760a9611230455b1f069f427b6b4f2
Somnath Paul <Somnath4x@gmail.com>
2020-05-11 14:26:29 +0530

    modified spider

--- crawl/crawl/spiders/quotes_spider.py
+++ crawl/crawl/spiders/quotes_spider.py
@@ -13,8 +13,5 @@ class QuotesSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        title = response.css('title::text').extract()
+        yield {'title': title }
